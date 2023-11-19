ee2b15ee274a50d95f5bd096555ca0ae935db384
Mistyttm <emmey.leo@gmail.com>
2023-11-19 22:38:55 +1000

    add new tests

--- __tests__/caesar.test.js
+++ __tests__/caesar.test.js
@@ -27,3 +27,31 @@ test("ceasar cipher encode 'xyz XYZ' shift 1", () => {
 test("ceasar cipher encode 'This is a test!' shift 5", () => {
     expect(ceasar("This is a test!", 5, true)).toBe("Ymnx nx f yjxy!");
 });
+
+test("ceasar cipher decode 'bcd' shift 1", () => {
+    expect(ceasar("bcd", 1, false)).toBe("abc");
+});
+
+test("ceasar cipher decode 'BCD' shift 1", () => {
+    expect(ceasar("BCD", 1, false)).toBe("ABC");
+});
+
+test("ceasar cipher decode 'bcd BCD' shift 1", () => {
+    expect(ceasar("bcd BCD", 1, false)).toBe("abc ABC");
+});
+
+test("ceasar cipher decode 'yza' shift 1", () => {
+    expect(ceasar("yza", 1, false)).toBe("xyz");
+});
+
+test("ceasar cipher decode 'YZA' shift 1", () => {
+    expect(ceasar("YZA", 1, false)).toBe("XYZ");
+});
+
+test("ceasar cipher decode 'yza YZA' shift 1", () => {
+    expect(ceasar("yza YZA", 1, false)).toBe("xyz XYZ");
+});
+
+test("ceasar cipher decode 'Ymnx nx f yjxy!' shift 5", () => {
+    expect(ceasar("Ymnx nx f yjxy!", 5, false)).toBe("This is a test!");
+});
