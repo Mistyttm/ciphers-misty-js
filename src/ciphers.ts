28a1043e29ad4b42006d24c48871abd58cd11b80
Mistyttm <emmey.leo@gmail.com>
2023-11-19 22:38:47 +1000

    refactor the caesar cipher

--- src/ciphers.ts
+++ src/ciphers.ts
@@ -1,40 +1,30 @@
 export function ceasar(text: string, key: number, encode: boolean) {
-    const characterSet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    const characterSetUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    const textArray = text.split('');
+    function encodeCharacter(character: string, key: number, isUpperCase: boolean, isEncode: boolean){
+        const base = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+        const offset = character.charCodeAt(0) - base;
+        let encodedOffset = 0;
+        if (isEncode){
+            encodedOffset = (offset + key) % 26;
+            return String.fromCharCode(encodedOffset + base);
+        } else {
+            encodedOffset = (offset - key + 26) % 26;
+            return String.fromCharCode(encodedOffset + base);
+        }
+        
+    }
 
     let message = "";
-
+    // Regex for non letters
     const specialRegex = /[\d\W\s_]/g;
-    switch(encode){
-        case true:
-            for (const character of textArray){
-                if (specialRegex.test(character)){
-                    message += character;
-                } else if (characterSet.includes(character)){
-                    let index = characterSet.indexOf(character);
-                    if ((index + key) > 25) {
-                        index = (index + key) % 26;
-                    } else {
-                        index = index + key
-                    }
-                    message += characterSet[index];
-                } else {
-                    let index = characterSetUpper.indexOf(character);
-                    if ((index + key) > 25) {
-                        index = (index + key) % 26;
-                    } else {
-                        index = index + key
-                    }
-                    message += characterSetUpper[index];
-                }
-            }
-            break;
-        case false:
-            message = "decode"
-            break;
+    for (const character of text){
+        if (specialRegex.test(character)){
+            message += character;
+        } else if (character >= 'a' && character <= 'z'){
+            message += encodeCharacter(character, key, false, encode);
+        } else if (character >= 'A' && character <= 'Z') {
+            message += encodeCharacter(character, key, true, encode);
+        }
     }
-    
 
     return message;
 }
