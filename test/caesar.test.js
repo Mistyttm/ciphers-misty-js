050a7e4b3e2d5cd9bcf348f3f9249ad463aefd00
Mistyttm <emmey.leo@gmail.com>
2023-11-20 00:09:59 +1000

    fix tests

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,36 +1,36 @@
-let cipher = require('../src/ciphers');
+const { ceasar } = require('../src/ciphers');
 let assert = require('assert');
 
 describe('Caesar', function () {
     describe('Encode', function () {
         describe('Key: 1', function () {
             it('Should return "bcd"', function () {
-                expect(cipher.ceasar("abc", 1, true)).toBe("bcd");
+                assert.equal(ceasar("abc", 1, true), "bcd");
             });
 
             it("should encode 'ABC' with shift 1", () => {
-                expect(cipher.ceasar("ABC", 1, true)).toBe("BCD");
+                assert.equal(ceasar("ABC", 1, true), "BCD");
             });
     
             it("should encode 'abc ABC' with shift 1", () => {
-                expect(cipher.ceasar("abc ABC", 1, true)).toBe("bcd BCD");
+                assert.equal(ceasar("abc ABC", 1, true), "bcd BCD");
             });
     
             it("should encode 'xyz' with shift 1", () => {
-                expect(cipher.ceasar("xyz", 1, true)).toBe("yza");
+                assert.equal(ceasar("xyz", 1, true), "yza");
             });
     
             it("should encode 'XYZ' with shift 1", () => {
-                expect(cipher.ceasar("XYZ", 1, true)).toBe("YZA");
+                assert.equal(ceasar("XYZ", 1, true), "YZA");
             });
     
             it("should encode 'xyz XYZ' with shift 1", () => {
-                expect(cipher.ceasar("xyz XYZ", 1, true)).toBe("yza YZA");
+                assert.equal(ceasar("xyz XYZ", 1, true), "yza YZA");
             });
         });
         describe('Key: 5', function () {
             it("should encode 'This is a test!' with shift 5", () => {
-                expect(cipher.ceasar("This is a test!", 5, true)).toBe("Ymnx nx f yjxy!");
+                assert.equal(ceasar("This is a test!", 5, true), "Ymnx nx f yjxy!");
             });
         });
     });
@@ -38,32 +38,32 @@ describe('Caesar', function () {
     describe('Decode', function () {
         describe('Key: 1', function () {
             it('Should return "abc"', function () {
-                expect(cipher.ceasar("bcd", 1, false)).toBe("abc");
+                assert.equal(ceasar("bcd", 1, false), "abc");
             });
 
             it("should decode 'BCD' with shift 1", () => {
-                expect(cipher.ceasar("BCD", 1, false)).toBe("ABC");
+                assert.equal(ceasar("BCD", 1, false), "ABC");
             });
     
             it("should decode 'bcd BCD' with shift 1", () => {
-                expect(cipher.ceasar("bcd BCD", 1, false)).toBe("abc ABC");
+                assert.equal(ceasar("bcd BCD", 1, false), "abc ABC");
             });
     
             it("should decode 'yza' with shift 1", () => {
-                expect(cipher.ceasar("yza", 1, false)).toBe("xyz");
+                assert.equal(ceasar("yza", 1, false), "xyz");
             });
     
             it("should decode 'YZA' with shift 1", () => {
-                expect(cipher.ceasar("YZA", 1, false)).toBe("XYZ");
+                assert.equal(ceasar("YZA", 1, false), "XYZ");
             });
     
             it("should decode 'yza YZA' with shift 1", () => {
-                expect(cipher.ceasar("yza YZA", 1, false)).toBe("xyz XYZ");
+                assert.equal(ceasar("yza YZA", 1, false), "xyz XYZ");
             });
         });
         describe('Key: 5', function () {
             it("should decode 'Ymnx nx f yjxy!' with shift 5", () => {
-                expect(cipher.ceasar("Ymnx nx f yjxy!", 5, false)).toBe("This is a test!");
+                assert.equal(ceasar("Ymnx nx f yjxy!", 5, false), "This is a test!");
             });
         });
     });
--- test/vigenere.test.js
+++ test/vigenere.test.js
@@ -1,27 +1,24 @@
-let ciphers = require('../src/ciphers');
+const { vigenere } = require('../src/ciphers');
 let assert = require('assert');
 
 describe('Vigenere', function () {
     describe('Encode', function () {
         describe('Keyword: TEST', function () {
             it('Should return "Mlal bw s mxwl iavslx"', function () {
-                assert.equal(ciphers.vigenere("This is a test phrase", "TEST", true), "Mlal bw s mxwl iavslx");
+                assert.equal(vigenere("This is a test phrase", "TEST", true), "Mlal bw s mxwl iavslx");
             });
         });
         describe('Keyword: TESTING', function () {
             it('Should return "Mlal qf g mikm xuxtww"', function () {
-                assert.equal(ciphers.vigenere("This is a test phrase", "TESTING", true), "Mlal qf g mikm xuxtww")
+                assert.equal(vigenere("This is a test phrase", "TESTING", true), "Mlal qf g mikm xuxtww")
             });
         });
     });
-});
-
-describe('Vigenere', function () {
     describe('Decode', function () {
         describe('Keyword: TEST', function () {
             it('Should return "This is a test phrase"', function () {
-                assert.equal(ciphers.vigenere("Mlal bw s mxwl iavslx", "TEST", false), "This is a test phrase")
-            })
-        })
-    })
+                assert.equal(vigenere("Mlal bw s mxwl iavslx", "TEST", false), "This is a test phrase");
+            });
+        });
+    });
 });
