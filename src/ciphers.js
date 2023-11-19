53dcdba7d383786e635352e2328a5507268ddac6
Mistyttm <emmey.leo@gmail.com>
2023-11-19 23:57:12 +1000

    remove error check for uppercase replacement

--- src/ciphers.js
+++ src/ciphers.js
@@ -1,28 +1,27 @@
 // Regex for non letters
 const specialRegex = /[\d\W\s_]/g;
 
-function encodeCharacter(character, key, isUpperCase, isEncode){
-    const base = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+function encodeCharacter(character, key, isUpperCase, isEncode) {
+    const base = isUpperCase ? "A".charCodeAt(0) : "a".charCodeAt(0);
     const offset = character.charCodeAt(0) - base;
     let encodedOffset = 0;
-    if (isEncode){
+    if (isEncode) {
         encodedOffset = (offset + key) % 26;
         return String.fromCharCode(encodedOffset + base);
     } else {
         encodedOffset = (offset - key + 26) % 26;
         return String.fromCharCode(encodedOffset + base);
     }
-    
 }
 
 export function ceasar(text, key, encode) {
     let message = "";
-    for (const character of text){
-        if (specialRegex.test(character)){
+    for (const character of text) {
+        if (specialRegex.test(character)) {
             message += character;
-        } else if (character >= 'a' && character <= 'z'){
+        } else if (character >= "a" && character <= "z") {
             message += encodeCharacter(character, key, false, encode);
-        } else if (character >= 'A' && character <= 'Z') {
+        } else if (character >= "A" && character <= "Z") {
             message += encodeCharacter(character, key, true, encode);
         }
     }
@@ -31,21 +30,28 @@ export function ceasar(text, key, encode) {
 }
 
 export function vigenere(text, keyword, encode) {
-
-    if (keyword === keyword.toLowerCase()){
-        return 0;
+    if (keyword === keyword.toLowerCase()) {
+        keyword = keyword.toUpperCase();
     }
 
-    let message = ""
+    let message = "";
 
     let keywordIndex = 0;
-    for (const character of text){
-        if (specialRegex.test(character)){
+    for (const character of text) {
+        if (specialRegex.test(character)) {
             message += character;
         } else {
-            const key = keyword[keywordIndex % keyword.length].toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);
-            
-            message += encodeCharacter(character, key, character === character.toUpperCase(), encode);
+            const key =
+                keyword[keywordIndex % keyword.length]
+                    .toLowerCase()
+                    .charCodeAt(0) - "a".charCodeAt(0);
+
+            message += encodeCharacter(
+                character,
+                key,
+                character === character.toUpperCase(),
+                encode
+            );
             keywordIndex++;
         }
     }
